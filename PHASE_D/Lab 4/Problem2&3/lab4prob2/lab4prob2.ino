ee5b440b6c735cae985c0945ab2d575331fbe551
luciano henriquez <luciano.henriquez@hotmail.com>
2025-03-05 18:09:50 -0700

    Added system diagram, I had initially only added code block diagram.

--- PHASE_D/Lab 4/Problem2&3/lab4prob2/lab4prob2.ino
+++ PHASE_D/Lab 4/Problem2&3/lab4prob2/lab4prob2.ino
@@ -5,11 +5,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 32  // OLED display height, in pixels
 
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
@@ -30,35 +30,35 @@ chargeStatus currentStatus = BATTERY;
 chargeStatus *current = &currentStatus;
 
 
-double readBatteryVoltage(){
-  return 5*analogRead(BATTERYVOLTAGE)/1023;
+double readBatteryVoltage() {
+  return 5 * analogRead(BATTERYVOLTAGE) / 1023;
 }
 
-double readPanelVoltage(){
-  return 5*analogRead(PANELVOLTAGE)/1023;
+double readPanelVoltage() {
+  return 5 * analogRead(PANELVOLTAGE) / 1023;
 }
 
-void pickPowerSource(chargeStatus *choice){
+void pickPowerSource(chargeStatus *choice) {
   double batteryVoltage = readBatteryVoltage();
   double panelVoltage = readBatteryVoltage();
   // double batteryVoltage = 3;
   // double panelVoltage = 4;
-  switch(*choice){
+  switch (*choice) {
     case BATTERY:
       digitalWrite(mosfetPanel, LOW);
       digitalWrite(mosfetBattery, HIGH);
-      if (panelVoltage >= 2.5 && batteryVoltage > panelVoltage){
+      if (panelVoltage >= 2.5 && batteryVoltage > panelVoltage) {
         *choice = PANEL;
-      } else if (batteryVoltage < panelVoltage){
+      } else if (batteryVoltage < panelVoltage) {
         *choice = CHARGING;
       }
       break;
     case PANEL:
       digitalWrite(mosfetBattery, LOW);
       digitalWrite(mosfetPanel, HIGH);
-      if (panelVoltage < 2.5 && batteryVoltage > panelVoltage){
+      if (panelVoltage < 2.5 && batteryVoltage > panelVoltage) {
         *choice = BATTERY;
-      } else if (batteryVoltage<panelVoltage){
+      } else if (batteryVoltage < panelVoltage) {
         *choice = CHARGING;
       }
       break;
@@ -66,36 +66,35 @@ void pickPowerSource(chargeStatus *choice){
       digitalWrite(mosfetBattery, LOW);
       digitalWrite(mosfetPanel, HIGH);
       digitalWrite(chargeBatteryPin, HIGH);
-      if (batteryVoltage > 4.9 && panelVoltage>=2.5){
+      if (batteryVoltage > 4.9 && panelVoltage >= 2.5) {
         *choice = PANEL;
-      } else if (batteryVoltage > 4.9 && panelVoltage<2.5){
+      } else if (batteryVoltage > 4.9 && panelVoltage < 2.5) {
         *choice = BATTERY;
       }
 
 
       break;
-
   }
 }
 
-void chargeAnimation(int i){
-    display.clearDisplay();
-    display.setCursor(8,10);
-    display.drawBitmap(0,0,images[i], SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
-    display.print("CHARGING");
-    display.display();
-    delay(100);
+void chargeAnimation(int i) {
+  display.clearDisplay();
+  display.setCursor(8, 10);
+  display.drawBitmap(0, 0, images[i], SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+  display.print("CHARGING");
+  display.display();
+  delay(100);
 }
 
-void drawOLED(chargeStatus *choice){
+void drawOLED(chargeStatus *choice) {
   display.setTextColor(SSD1306_WHITE);
   display.setTextSize(1);
-  display.setCursor(8,10);
-  if (*choice==BATTERY){
-    display.drawBitmap(0,0,images[1], SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+  display.setCursor(8, 10);
+  if (*choice == BATTERY) {
+    display.drawBitmap(0, 0, images[1], SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
     display.print("BATTERY");
-  } else if (*choice == PANEL){
-    display.drawBitmap(0,0,images[0], SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+  } else if (*choice == PANEL) {
+    display.drawBitmap(0, 0, images[0], SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
     display.print("SOLAR PANEL");
   } else {
     chargeAnimation(4);
@@ -103,10 +102,7 @@ void drawOLED(chargeStatus *choice){
     chargeAnimation(2);
     chargeAnimation(1);
     delay(300);
-
-    
   }
-
 }
 
 
@@ -116,9 +112,10 @@ void drawOLED(chargeStatus *choice){
 void setup() {
   Serial.begin(9600);
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ;  // Don't proceed, loop forever
   }
 
   // Show initial display buffer contents on the screen --
@@ -133,9 +130,6 @@ void setup() {
   pinMode(chargeBatteryPin, OUTPUT);
   pinMode(mosfetBattery, OUTPUT);
   pinMode(mosfetPanel, OUTPUT);
-
-
-
 }
 
 void loop() {
@@ -143,9 +137,4 @@ void loop() {
   pickPowerSource(current);
   drawOLED(current);
   display.display();
-
-  
-
 }
-
-
